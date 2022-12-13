922eda498a52df73b58751bb1b366e34fd7bd0cb
Gasteri <tipzov@yandex.ru>
2022-12-13 11:32:04 +0300

    add inverted array

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using static Common.Helper;
+
+
+int[] array = CreateRandomArray();
+PrintArray(array);
+Console.WriteLine();
+int[] reversed = ReverseArray(array);
+PrintArray(array);
+int[] ReverseArray(int[] array)
+{
+    int[] reversedArray = new int[array.Length];
+
+    for (var i = 0; i < array.Length; i++)
+    {
+        reversedArray[i] = array[array.Length - 1 - i];
+    }
+
+    return reversedArray;
+}
+
