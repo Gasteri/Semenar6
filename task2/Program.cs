954698adbe26c2d6480228bdab3612093809c87b
Gasteri <tipzov@yandex.ru>
2022-12-13 23:06:48 +0300

    add binary number

---
+++ task2/Program.cs
@@ -0,0 +1,65 @@
+﻿using static Common.Helper;
+/*Задача 42: 
+Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+ 45 -> 101101 , 3 -> 11 , 2 -> 10*/
+int number = InputInt();
+int InputInt()
+{
+    bool isParsed = int.TryParse(Console.ReadLine(), out int num);
+
+    if (!isParsed)
+    {
+        Console.WriteLine("Incorrect number");
+        return -1;
+    }
+    else
+    {
+        return num;
+    }
+}
+int[] binNumber = TenToBin(number);
+PrintArray1(binNumber);
+int[] TenToBin(int num)
+{
+    int numRange = 0;
+    int numTemp = num;  
+    while (numTemp > 0)
+    { 
+        numTemp /= 2;
+        numRange++;
+        Console.WriteLine(numRange);
+    }
+    int length = numRange;
+    //return numRange;
+    int[] arr = new int[length];
+    for(int i = length - 1;i >= 0; i--)
+    {
+        arr[i] = num % 2;
+        num = num / 2;
+    }
+    
+    return arr;
+}
+
+/*string GetttoBin(int number) //преобразование через строчку
+{
+    string binar = string.Empty;
+    while(number != 0)
+    {
+        binar = number % 2 + binar;
+        number /= 2;
+    }
+    return binar;
+}*/
+
+/*int a = InputInt();
+int b = InputInt();
+int c = InputInt();
+
+bool res = IsTriangle(a, b, c);
+Console.WriteLine(res);
+
+bool IsTriangle(int a, int b, int c)
+{
+    return(a <(b + c) && b < (a + c) && c < (a + b));
+}*/
